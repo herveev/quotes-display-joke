a7f573451bbaea7b5c3f63946bf756102e5d3c9f
Hervé EVODA <herveevoda@gmail.com>
2022-03-10 22:41:05 +0100

    Create Spring Boot Project for the Joke.

---
+++ src/main/java/com/pitoscode/quotesjokes/QuotesJokesApplication.java
@@ -0,0 +1,13 @@
+package com.pitoscode.quotesjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotesJokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pitoscode/quotesjokes/QuotesJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitoscode.quotesjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
